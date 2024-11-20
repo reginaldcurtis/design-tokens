11d33814c08d7ba380ef4742b872fcbd49feb22f
Reginald Curtis <reginald.curtis@tangocard.com>
2024-11-20 09:29:30 -0600

    base files

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Button() {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
+  return (
+    <button onClick={handleClick}>Click Me</button>
+  );
+}
+
+export default Button;
